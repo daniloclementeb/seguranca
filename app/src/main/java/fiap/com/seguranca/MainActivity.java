291d21fda7bf5ea955ab47d59de73b8f446eb8da
daniloclementeb <danilo.clementeb@gmail.com>
2018-04-15 17:58:43 -0300

    Evidencias

---
+++ app/src/main/java/fiap/com/seguranca/MainActivity.java
@@ -0,0 +1,11 @@
+package fiap.com.seguranca;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((int) R.layout.activity_main);
+    }
+}
